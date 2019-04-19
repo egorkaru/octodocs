e8a2d2daab55418d08b56ed23760dd0e78532bf9
Egor Rudinskiy <rudgv2008@gmail.com>
2019-04-19 14:08:42 +0300

    babel

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import { getAppConfig } from './utils/config';
 import { getYALM, listServices } from './api/api';
 import { parse } from 'url'
-import * as next from 'next'
 import { IncomingMessage, ServerResponse } from 'http';
 import { routes } from './api/routes';
 import { createProxyServer } from 'http-proxy'
@@ -11,17 +10,12 @@ const config = getAppConfig()
 
 const dev = process.env.NODE_ENV !== 'production'
 
-const app = next({ dev, dir: `${__dirname}` })
-const handle = app.getRequestHandler()
-
 const proxy = createProxyServer()
 
 const isAPI = (req: IncomingMessage) => match(req, '/api/*')
 const isOctodocsApi = (req: IncomingMessage) => match(req, '/api/octodocs/*')
 const isList = (req: IncomingMessage) => match(req, routes.list())
 const isGetYAML = (req: IncomingMessage) => match(req, routes.yaml(':service'))
-
-const isViewPage = (req: IncomingMessage) => match(req, '/view/:service')
 
 async function main (req: IncomingMessage, res: ServerResponse) {
   const parsedUrl = parse(req.url || '', true)
@@ -34,10 +28,6 @@ async function main (req: IncomingMessage, res: ServerResponse) {
     }
     return proxy.web(req, res, { target: config.url, headers: { Host: parse(config.url).host || config.url } })
   }
-  if (isViewPage(req)) {
-    return app.render(req, res, '/view', isViewPage(req).params, parsedUrl)
-  }
-  return handle(req, res, parsedUrl)
 }
 
 async function setup (handler: typeof main) {

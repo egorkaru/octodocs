415c85cd69a49ccd254ba19d7ad6c45df8c02a58
Egor Rudinsky <rudgv2008@gmail.com>
2019-04-19 11:47:06 +0300

    Woking SwaggerUI "try it out" feature (#3)

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,23 @@
+import { ConfigJSON } from './model/ConfigJSON';
 import { getYALM, listServices } from './api/api';
 import { parse } from 'url'
 import * as next from 'next'
 import { IncomingMessage, ServerResponse } from 'http';
 import { routes } from './api/routes';
+import { createProxyServer } from 'http-proxy'
+
 const match = require('micro-route/match')
+const config : ConfigJSON = require('../config.json')
 
 const dev = process.env.NODE_ENV !== 'production'
 
 const app = next({ dev, dir: `${__dirname}` })
 const handle = app.getRequestHandler()
 
-const isAPI = (req: IncomingMessage) => match(req, '/api/octodocs/*')
+const proxy = createProxyServer()
+
+const isAPI = (req: IncomingMessage) => match(req, '/api/*')
+const isOctodocsApi = (req: IncomingMessage) => match(req, '/api/octodocs/*')
 const isList = (req: IncomingMessage) => match(req, routes.list())
 const isGetYAML = (req: IncomingMessage) => match(req, routes.yaml(':service'))
 
@@ -19,10 +26,13 @@ const isViewPage = (req: IncomingMessage) => match(req, '/view/:service')
 async function main (req: IncomingMessage, res: ServerResponse) {
   const parsedUrl = parse(req.url || '', true)
   if (isAPI(req)) {
-    const listRoute = isList(req)
-    const getYAMLRoute = isGetYAML(req)
-    if (listRoute) return listServices(res)
-    if (getYAMLRoute && getYAMLRoute.params.service) return getYALM(res, getYAMLRoute.params.service)
+    if (isOctodocsApi(req)) {
+      const listRoute = isList(req)
+      const getYAMLRoute = isGetYAML(req)
+      if (listRoute) return listServices(res)
+      if (getYAMLRoute && getYAMLRoute.params.service) return getYALM(res, getYAMLRoute.params.service)
+    }
+    return proxy.web(req, res, { target: config.url, headers: { Host: parse(config.url).host || config.url } })
   }
   if (isViewPage(req)) {
     return app.render(req, res, '/view', isViewPage(req).params, parsedUrl)
